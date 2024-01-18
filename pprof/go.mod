b267b59db5e320060359a81116c79cf5e364bd34
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2024-01-18 12:32:51 +0700

    rewrite test

--- pprof/go.mod
+++ pprof/go.mod
@@ -3,14 +3,22 @@ module github.com/grafana/jfr-parser/pprof
 go 1.18
 
 require (
+	github.com/google/pprof v0.0.0-20240117000934-35fc243c5815
 	github.com/grafana/jfr-parser v0.7.2-0.20230831140626-08fa3a941bf8
 	github.com/grafana/pyroscope/api v0.3.0
+	github.com/k0kubun/pp/v3 v3.2.0
 	github.com/stretchr/testify v1.8.4
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20230524184225-eabc099b10ab // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pprof/parser_test.go
@@ -0,0 +1,267 @@
+package pprof
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"slices"
+	"strings"
+	"testing"
+
+	gpprof "github.com/google/pprof/profile"
+	profilev1 "github.com/grafana/pyroscope/api/gen/proto/go/google/v1"
+	"github.com/k0kubun/pp/v3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const testdataDir = "../parser/testdata/"
+const doDump = false
+
+type testdata struct {
+	jfr, labels   string
+	expectedCount int
+}
+
+var testfiles = []testdata{
+	{"example", "", 4},
+	{"async-profiler", "", 3}, // -e cpu -i 10ms --alloc 512k --wall 200ms --lock 10ms -d 60 (async-profiler 2.10)
+	{"goland", "", 5},
+	{"goland-multichunk", "", 5},
+	//{"FastSlow_2024_01_16_180855", "", 0},// from IJ Ultimate, multichunk, chunked CP
+	{"cortex-dev-01__kafka-0__cpu__0", "", 1},
+	{"cortex-dev-01__kafka-0__cpu__1", "", 1},
+	{"cortex-dev-01__kafka-0__cpu__2", "", 1},
+	{"cortex-dev-01__kafka-0__cpu__3", "", 1},
+	{"cortex-dev-01__kafka-0__cpu_lock0_alloc0__0", "", 5},
+	{"cortex-dev-01__kafka-0__cpu_lock_alloc__0", "", 2},
+	{"cortex-dev-01__kafka-0__cpu_lock_alloc__1", "", 2},
+	{"cortex-dev-01__kafka-0__cpu_lock_alloc__2", "", 2},
+	{"cortex-dev-01__kafka-0__cpu_lock_alloc__3", "", 2},
+	{"dump1", "dump1.labels.pb.gz", 1},
+	{"dump2", "dump2.labels.pb.gz", 4},
+}
+
+type gprofile struct {
+	profile *gpprof.Profile
+	proto   *profilev1.Profile
+	metric  string
+}
+
+func TestDoDump(t *testing.T) {
+	assert.False(t, doDump)
+}
+
+var parseInput = &ParseInput{
+	SampleRate: 100,
+}
+
+func TestParse(t *testing.T) {
+	for _, testfile := range testfiles {
+		mypp := pp.New()
+		mypp.SetColoringEnabled(false)
+		mypp.SetExportedOnly(true)
+		t.Run(testfile.jfr, func(t *testing.T) {
+			jfrFile := testdataDir + testfile.jfr + ".jfr.gz"
+			jfr := readGzipFile(t, jfrFile)
+			ls := readLabels(t, testfile)
+
+			profiles, err := ParseJFR(jfr, parseInput, ls)
+			require.NoError(t, err)
+
+			gprofiles := toGoogleProfiles(t, profiles.Profiles)
+			profiles = nil
+
+			slices.SortFunc(gprofiles, func(i, j gprofile) int {
+				return strings.Compare(i.metric, j.metric)
+			})
+			assert.Equal(t, testfile.expectedCount, len(gprofiles))
+
+			for i, profile := range gprofiles {
+				actual := profile.profile.String()
+				actualCollapsed := stackCollapseProto(profile.proto, false)
+				expectedFile := fmt.Sprintf("%s%s_%d_%s_expected.txt.gz", testdataDir, testfile.jfr, i, profile.metric)
+				expectedCollapsedFile := fmt.Sprintf("%s%s_%d_%s_expected_collapsed.txt.gz", testdataDir, testfile.jfr, i, profile.metric)
+				assert.NotEmpty(t, actual)
+				assert.NotEmpty(t, actualCollapsed)
+				if doDump {
+					writeGzipFile(t, expectedFile, []byte(actual))
+					writeGzipFile(t, expectedCollapsedFile, []byte(actualCollapsed))
+				} else {
+					expected := readGzipFile(t, expectedFile)
+					require.NoError(t, err)
+					expectedCollapsed := readGzipFile(t, expectedCollapsedFile)
+					require.NoError(t, err)
+
+					assert.Equal(t, string(expected), actual)
+					assert.Equal(t, string(expectedCollapsed), actualCollapsed)
+
+					if string(expected) != actual {
+						os.WriteFile("actual.txt", []byte(actual), 0644)
+						os.WriteFile("expected.txt", expected, 0644)
+					}
+
+					if string(expectedCollapsed) != actualCollapsed {
+						os.WriteFile("actual_collapsed.txt", []byte(actualCollapsed), 0644)
+						os.WriteFile("expected_collapsed.txt", expectedCollapsed, 0644)
+					}
+				}
+			}
+		})
+	}
+}
+
+func readLabels(t testing.TB, td testdata) *LabelsSnapshot {
+	ls := new(LabelsSnapshot)
+	if td.labels != "" {
+		labelsBytes := readGzipFile(t, testdataDir+td.labels)
+		err := ls.UnmarshalVT(labelsBytes)
+		require.NoError(t, err)
+	}
+	return ls
+}
+
+func BenchmarkParse(b *testing.B) {
+	for _, testfile := range testfiles {
+		b.Run(testfile.jfr, func(b *testing.B) {
+			jfr := readGzipFile(b, testdataDir+testfile.jfr+".jfr.gz")
+			ls := readLabels(b, testfile)
+			b.ResetTimer()
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				profiles, err := ParseJFR(jfr, parseInput, ls)
+				if err != nil {
+					b.Fatalf("Unable to parse JFR file: %s", err)
+				}
+				if len(profiles.Profiles) == 0 {
+					b.Fatalf("No profiles found")
+				}
+			}
+		})
+	}
+}
+
+func toGoogleProfiles(t *testing.T, profiles []Profile) []gprofile {
+	res := make([]gprofile, 0, len(profiles))
+	for _, profile := range profiles {
+		bs, err := profile.Profile.MarshalVT()
+		require.NoError(t, err)
+		p, err := gpprof.ParseData(bs)
+		require.NoError(t, err)
+
+		res = append(res, gprofile{p, profile.Profile, fmt.Sprintf("%s_%s", profile.Metric, sampleTypesToString(p))})
+	}
+	return res
+}
+
+func sampleTypesToString(p *gpprof.Profile) string {
+	var sh1 string
+	for _, s := range p.SampleType {
+		dflt := ""
+		sh1 = sh1 + fmt.Sprintf("%s__%s%s ", s.Type, s.Unit, dflt)
+	}
+	return strings.TrimSpace(sh1)
+}
+
+func readGzipFile(t testing.TB, fname string) []byte {
+	f, err := os.Open(fname)
+	require.NoError(t, err)
+	defer f.Close()
+	r, err := gzip.NewReader(f)
+	require.NoError(t, err)
+	defer r.Close()
+	bs, err := ioutil.ReadAll(r)
+	require.NoError(t, err)
+	return bs
+}
+
+func writeGzipFile(t *testing.T, f string, data []byte) {
+	fd, err := os.OpenFile(f, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	require.NoError(t, err)
+	defer fd.Close()
+	g := gzip.NewWriter(fd)
+	_, err = g.Write(data)
+	require.NoError(t, err)
+	err = g.Close()
+	require.NoError(t, err)
+}
+
+func stackCollapseProto(p *profilev1.Profile, lineNumbers bool) string {
+	allZeros := func(a []int64) bool {
+		for _, v := range a {
+			if v != 0 {
+				return false
+			}
+		}
+		return true
+	}
+	addValues := func(a, b []int64) {
+		for i := range a {
+			a[i] += b[i]
+		}
+	}
+
+	type stack struct {
+		funcs string
+		value []int64
+	}
+	locMap := make(map[int64]*profilev1.Location)
+	funcMap := make(map[int64]*profilev1.Function)
+	for _, l := range p.Location {
+		locMap[int64(l.Id)] = l
+	}
+	for _, f := range p.Function {
+		funcMap[int64(f.Id)] = f
+	}
+
+	var ret []stack
+	for _, s := range p.Sample {
+		var funcs []string
+		for i := range s.LocationId {
+			locID := s.LocationId[len(s.LocationId)-1-i]
+			loc := locMap[int64(locID)]
+			for _, line := range loc.Line {
+				f := funcMap[int64(line.FunctionId)]
+				fname := p.StringTable[f.Name]
+				if lineNumbers {
+					fname = fmt.Sprintf("%s:%d", fname, line.Line)
+				}
+				funcs = append(funcs, fname)
+			}
+		}
+
+		vv := make([]int64, len(s.Value))
+		copy(vv, s.Value)
+		ret = append(ret, stack{
+			funcs: strings.Join(funcs, ";"),
+			value: vv,
+		})
+	}
+	slices.SortFunc(ret, func(i, j stack) int {
+		return strings.Compare(i.funcs, j.funcs)
+	})
+	var unique []stack
+	for _, s := range ret {
+		if allZeros(s.value) {
+			continue
+		}
+		if len(unique) == 0 {
+			unique = append(unique, s)
+			continue
+		}
+
+		if unique[len(unique)-1].funcs == s.funcs {
+			addValues(unique[len(unique)-1].value, s.value)
+			continue
+		}
+		unique = append(unique, s)
+
+	}
+
+	res := make([]string, 0, len(unique))
+	for _, s := range unique {
+		res = append(res, fmt.Sprintf("%s %v", s.funcs, s.value))
+	}
+	return strings.Join(res, "\n")
+}
